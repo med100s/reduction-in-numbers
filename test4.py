4d9a145f2eb0b0cf908c50af7c256b7040101bda
med100s <61676013+med100s@users.noreply.github.com>
2020-10-17 20:26:21 +0500

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,46 @@
+
+def preobr(x):
+	r = [x[0]]
+	i = 1
+	j = 0
+	while i < len(x):
+	    if r[j] != x[i]:
+	        r.append(x[i])
+	        j += 1
+	    i += 1
+	return r
+
+def solution(args):
+	z=[]
+	x=[]
+	N = len(args)
+	for j in range(N-2):
+		try:
+			if args[j+1]-args[j]==1 and args[j+2]-args[j+1]==1:
+				z.append(args[j+1])
+		except IndexError:
+			print(1)
+	ss = []
+	for e in args:
+		if not e in z:
+			ss.append(e)
+		else:
+			ss.append('-')
+	alp = preobr(ss)
+	c = 0
+	while c < len(alp)-1 :
+		if type(alp[c]) is int and type(alp[c+1]) is int:
+				alp.insert(c+1,',')
+		c =c + 1
+		pass
+	
+	makeitastring = ''.join(map(str, alp)) 
+	print(makeitastring)
+	return makeitastring
+
+solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11])
+solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20,21,2453,34523,2345234,36435634,1232413525])
+solution([-3,-2,-1,2,10,15,16,18,19,20])
+solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20])
+solution([1,2,3,4,5])
+solution([1,2,3,4,5])
